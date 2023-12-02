1438b0d2160778c118988bf3dcc774c6f8050747
Philipp Leppert <philippleppert@web.de>
2023-12-02 22:44:25 +0100

    UI changes

--- app.R
+++ app.R
@@ -7,38 +7,43 @@ data <- read.xlsx("disney_filme.xlsx")
 
 ui <- fluidPage(
   fluidRow(
-    column(6, h1("Disney++")),
+    column(6, h1("Disney-Dice")),
     column(6, img(src='logo.png', align = "right", width = "150"))
   ),
   br(),
   sidebarLayout(
     sidebarPanel( actionButton("button", "Hey Cri-Kee, schmeiß' die Losfee an!")),
     mainPanel(
-      textOutput("chosen_film")
+      br(),
+      h4(textOutput("chosen_film"))
       )
   )
 )
 
 server <- function(input, output, session) {
   
+  # store all rolled movies in session
   rvals <- reactiveValues(
     rolled_movies = NULL
   )
   
+  # observe action button
   observeEvent(input$button, ignoreInit = T, {
     
+    # currently rolled movie
     rolled_movie <- 
       data %>% 
       filter(!(film %in% rvals$rolled_movies)) %>%
       slice_sample(n = 1, replace = F) %>% 
       pull(film) 
    
+    # add to reactive 
    rvals$rolled_movies <- c(rvals$rolled_movies, rolled_movie )
    
-   print(rvals$rolled_movies)
-    output$chosen_film <- renderText({
-        str_c("Ihr schaut heute... ",  rolled_movie)
-    })
+   # output rolled movie
+   output$chosen_film <- renderText({
+     str_c("Ihr schaut heute... ",  rolled_movie)
+     })
     
   })
 
